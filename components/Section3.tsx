24086f61495d0a11d7db8dded21f82e409c741fd
Mimi <mimi@ycse.kr>
2026-05-13 01:25:02 +0900

    temp

--- components/Section3.tsx
+++ components/Section3.tsx
@@ -254,7 +254,7 @@ const Section3 = () => {
                 <div className="flex h-full flex-col">
                   <FeatureCardImage sources={feature.imageSources} alt={feature.title} />
 
-                  <div className="flex flex-1 flex-col bg-bg-blue px-6 py-7 md:px-8 md:py-9 lg:px-10 lg:py-11">
+                  <div className="flex flex-1 flex-col bg-bg-gray px-6 py-7 md:px-8 md:py-9 lg:px-10 lg:py-11">
                     <h6 className="type-h5 mb-2 text-yes-blue md:mb-[9px] lg:mb-[11px]">
                       {feature.title}
                     </h6>
